01a1bea7302a229760531ebc7e7462e52e6e884b
hmulya <hmulya@fintax.id>
2022-03-06 22:24:27 +0700

    add example conf from minindnedit

--- map/map.py
+++ map/map.py
@@ -1,20 +1,18 @@
-from turtle import distance
 import requests
 import os
 import random
+import json
 from math import sqrt, atan as arctan, cos, degrees, radians
+import networkx as nx
+import matplotlib.pyplot as plt
 
-class City:
-    def __init__(self, name, lat, lon):
-        self.name = str(name)
-        self.lat = float(lat)
-        self.lon = float(lon)
-
-def delta_km(city1: City, city2: City):
-    delta_lat = city1.lat - city2.lat
-    delta_lon = city1.lon - city2.lon
+def delta_km(city1: str, city2: str, data: dict):
+    lat1, lon1 = data[city1]["cartesian_coord"]
+    lat2, lon2 = data[city2]["cartesian_coord"]
+    delta_lat = lat1 - lat2
+    delta_lon = lon1 - lon2
     delta_lat_km = delta_lat * lat_conv
-    delta_lon_km = delta_lon * long_conv * cos(radians((city1.lat + city2.lat)/2))
+    delta_lon_km = delta_lon * long_conv * cos(radians((lat1 + lat2)/2))
     delta_km = sqrt(delta_lat_km**2 + delta_lon_km**2)
     return delta_km
 
@@ -24,34 +22,39 @@ long_conv  = 111.320
 if os.path.exists(outfile):
   os.remove(outfile)
 
-cities = [
-    "Jakarta",
-    "Bandung",
-    "Padang",
-    "Medan",
-    "Tasikmalaya",
-    "Purwokerto",
-    "Jogjakarta",
-    "Magelang",
-    "Semarang",
-    "Klaten",
-    "Jayapura",
-    "Makassar",
-    "Surabaya",
-    "Balikpapan",
-    "Manado",
-    "Palembang",
-    "Jayapura"
-]
-cities.sort()
+cities = {
+    "Medan": {"neighbors": ["Bengkulu", "Jambi", "Batam"]},
+    "Bengkulu": {"neighbors": ["Medan", "Lampung", "Palembang"]},
+    "Jambi": {"neighbors": ["Medan", "Palembang"]},
+    "Batam": {"neighbors": ["Medan", "Pontianak", "Jakarta", "Jambi"]},
+    "Palembang": {"neighbors": ["Bengkulu", "Jambi", "Lampung"]},
+    "Lampung": {"neighbors": ["Bengkulu", "Palembang"]},
+    "Jakarta": {"neighbors": ["Lampung", "Batam", "Pontianak", "Surabaya"]},
+    "Pontianak": {"neighbors": ["Batam", "Jakarta", "Banjarmasin"]},
+    "Surabaya": {"neighbors": ["Jakarta", "Banjarmasin", "Makassar", "Kupang"]},
+    "Banjarmasin": {"neighbors": ["Pontianak", "Surabaya", "Balikpapan", "Makassar"]},
+    "Balikpapan": {"neighbors": ["Banjarmasin", "Manado"]},
+    "Makassar": {"neighbors": ["Banjarmasin", "Surabaya"]},
+    "Kendari": {"neighbors": ["Makassar", "Manado", "Fakfak"]},
+    "Manado": {"neighbors": ["Balikpapan", "Kendari", "Ternate"]},
+    "Kupang": {"neighbors": ["Surabaya", "Tual"]},
+    "Ternate": {"neighbors": ["Manado", "Sorong", "Fakfak"]},
+    "Sorong": {"neighbors": ["Ternate", "Jayapura"]},
+    "Fakfak": {"neighbors": ["Ternate", "Kendari", "Tual", "Timika", "Jayapura"]},
+    "Tual": {"neighbors": ["Fakfak", "Kupang", "Timika"]},
+    "Timika": {"neighbors": ["Fakfak", "Tual"]},
+    "Jayapura": {"neighbors": ["Sorong", "Fakfak"]}
+}
 cities_info = []
 base_url = 'http://api.openweathermap.org/geo/1.0/direct'
+G = nx.Graph()
+pos = {}
 
 with open(outfile,"a") as file:
     file.write("[nodes]\n")
 for city in cities:
     params = {
-        'q': city,
+        'q': f'{city},ID',
         'limit': 5,
         'appid': '874f6acc3c0bad4f9c019e59f8d8c6bc'
         }
@@ -59,8 +62,10 @@ for city in cities:
     data = res.json()
     try:
         name = data[0]["local_names"]["id"]
-    except Exception as e:
+    except KeyError as e:
         name = data[0]["name"]
+    except Exception as e:
+        print(e)
     lat = data[0]["lat"]
     lon = data[0]["lon"]
     r = sqrt(lat**2 + lon**2)
@@ -70,13 +75,25 @@ for city in cities:
         # print(f'{name+", ":>20}lat: {lat:>7.7f}, lon: {lon:>7.7f}, r: {r:>14.14f}, Θ:{theta:>14.14f}')
     with open(outfile,"a") as file:
         file.write(f'{name}: _ radius={r:>14.14f} angle={theta:>14.14f}\n')
-    cities_info.append(City(name=name, lat=lat, lon=lon))
+    cities[city]["cartesian_coord"] = (lat,lon)
+    cities[city]["polar_coord"] = (r,theta)
+    G.add_node(city, pos=(lat,lon))
+    pos[city] = (lon,lat)
+
+with open("debug.json","w") as file:
+    json.dump(cities, file, indent=4)
 
 with open(outfile,"a") as file:
     file.write("\n[links]\n")
-for i,city1 in enumerate(cities_info):
-    for j,city2 in enumerate(cities_info):
-        if j > i:
-            distance = delta_km(city1, city2)
+
+checklist = []
+for src, value in cities.items():
+    for dst in value["neighbors"]:
+        if (src,dst) not in checklist and (dst,src) not in checklist:
+            checklist.append((src,dst))
+            G.add_edge(src,dst)
+            distance = delta_km(src, dst, cities)
             with open(outfile,"a") as file:
-                file.write(f'{city1.name}:{city2.name} delay={int(distance/299.792458+random.uniform(1,3))}ms\n')
+                file.write(f'{src}:{dst} delay={int(distance/299.792458+random.uniform(1,3))}ms\n')
+nx.draw(G,pos=pos,with_labels=True)
+plt.show()
